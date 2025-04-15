57493bafcdbcaaed9c531b3417b7193ad02e798a
Alexander Morgan <alexanderpmorgan@gmail.com>
2025-04-16 00:38:19 +0200

    Remove local files from train.py

--- train.py
+++ train.py
@@ -10,12 +10,12 @@ os.makedirs("models", exist_ok=True)
 # ~1GB of text compressed into key-value pairs of str: int. Words appearing fewer than 10 times were filtered out.
 data = ['./tests/1GB_of_FineWeb-Edu_10B_sample_freq_cutoff_10.csv']
 # # You can also train on a text file. See the README for more details on acceptable file formats.
-# data = "tests/taylorswift.txt"   # <- a copy of the wikipedia article on Taylor Swift
+taylor_swift_text = "tests/taylorswift.txt"   # <- a copy of the wikipedia article on Taylor Swift
 
-for TokenizerClass, name in zip([QuickTokenizer, BatchTokenizer], ['quick', 'batch']):
+for TokenizerClass, name in zip([BatchTokenizer, QuickTokenizer], ['batch', 'quick']):
     t0 = time.time()
-    tokenizer = TokenizerClass()
-    tokenizer.train(data, 10000, verbose=True)   # the more merges you do, the larger the average batch size will be
+    tokenizer = TokenizerClass(store_dict=False)
+    tokenizer.train(data, 50304, verbose=True)   # the more merges you do, the larger the average batch size will be
     t1 = time.time()
     # write name.model and name.vocab files in the models directory
     prefix = os.path.join("models", name)
